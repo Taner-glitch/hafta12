5b61bdad091593862fdb8bea5e56f0ab10d94ea3
Taner-glitch <taner.eren2003@gmail.com>
2025-05-10 13:33:22 +0300

    Update and rename README.md to udp_server.py

---
+++ udp_server.py
@@ -0,0 +1,34 @@
+import socket
+
+localIP = "127.0.0.1"
+localPort = 20001
+bufferSize = 1024
+
+UDPServerSocket = socket.socket(family = socket.AF_INET, type = socket.SOCK_DGRAM)
+UDPServerSocket.bind((localIP, localPort))
+print("UDP server up and listening")
+
+# this might be database or a file
+di = {'taner':'eren', '17BEC0647':'shikhar', '17BEC0150':'tanveer','17BCE2119':'sahil', '17BIT0123':'sidhant'}
+
+while(True):
+    name, addr1 = UDPServerSocket.recvfrom(bufferSize)
+    pwd, addr1 = UDPServerSocket.recvfrom(bufferSize)
+
+name = name.decode()
+pwd = pwd.decode()
+msg =''
+if name not in di:
+	msg ='name does not exists'
+	flag = 0
+
+for i in di:
+	if i == name:
+		if di[i]== pwd:
+			msg ="pwd match"
+			flag = 1
+		else:
+			msg ="pwd wrong"
+	bytesToSend = str.encode(msg)
+
+	UDPServerSocket.sendto(bytesToSend, addr1)
